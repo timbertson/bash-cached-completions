718745e987a403a165663f04d138ea60b06e79a0
gfxmonk <tim3d.junk@gmail.com>
2010-11-14 12:51:11 +1100

    added zsh support. did you know that the zsh completion API is actually sane? this may be the end of bash for me...

--- bashcomplete/compreply.py
+++ bashcomplete/compreply.py
@@ -3,10 +3,14 @@ import escape
 import os
 import sys
 
-def perform_completion(words, *a):
-	return escape.assign_to_bash_array('COMPLETION_WORDS', words + concat_colons(words, *a))
+def filter_matches(words, current_word):
+	return [word for word in words if word.startswith(current_word)]
 
 def concat_colons(words, current_index, argv):
+	"""hack around bash's splitting of http:// into ['http', ':', '//']
+	by joining up colons, matching against words, and putting
+	a modified version of each word in the completions so that it'll match
+	the part after the last colon"""
 	current_word = argv[current_index]
 	non_response_chars = 0
 	if any([':' in w for w in words]):
@@ -18,15 +22,8 @@ def concat_colons(words, current_index, argv):
 				non_response_chars += len(last_word) + 1
 			else:
 				break
-	reply = [word for word in words if word.startswith(current_word)]
+	reply = filter_matches(words, current_word)
 
 	if non_response_chars:
 		reply = map(lambda w: w[non_response_chars:], reply)
 	return reply
-
-if __name__ == '__main__':
-	argv = sys.argv[1:]
-	separator = os.environ.get("COMPLETION_WORDS_SEP", '\n')
-	words = os.environ['COMPLETION_WORDS'].split(separator)
-	current_index = int(argv.pop(0))
-	print perform_completion(words, current_index, argv)
